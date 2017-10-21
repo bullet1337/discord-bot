da33e1f19f9814adec4add76cf5c9784371d35b6
Asiryan Alexander <alichek95@mail.ru>
2017-10-21 19:04:07 +0300

    Replace Client with Bot

--- bot.py
+++ bot.py
@@ -1,43 +1,48 @@
 #!/usr/bin/python
-import discord
+import asyncio
 
-client = discord.Client()
+from discord.ext import commands
+from discord.ext.commands import Bot
+
 TOKEN = 'MzcwODcyNDAzNDAxOTAwMDMz.DMtZZg.SiNxXQ7nOWhrSTzMk8aFcJxJQIs'
-PREFIX = '!test'
+GREETINGS_DELAY = 0.1
+VOLUME = 0.1
+
+bot = Bot(command_prefix=commands.when_mentioned_or('play'))
 
 
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
 
-@client.event
-async def on_message(message):
-    if message.content.startswith(PREFIX):
-        await client.send_message(message.channel, 'echo:%s' % message.content.replace(PREFIX, '', 1))
+@bot.command()
+async def echo(message):
+    await bot.say('echo:%s' % message)
 
 
-@client.event
+@bot.event
 async def on_voice_state_update(before, after):
-    if not before.bot and before.voice.voice_channel != after.voice.voice_channel:
-        if client.player:
-            client.player.stop()
+    if not before.bot and after.voice.voice_channel and before.voice.voice_channel != after.voice.voice_channel:
+        if bot.player:
+            bot.player.stop()
         print('%s: %s -> %s' % (before.name, before.voice.voice_channel, after.voice.voice_channel))
-        if not client.connection.voice_clients:
-            client.voice_channel = await client.join_voice_channel(after.voice.voice_channel)
+        if not bot.connection.voice_clients:
+            bot.voice_channel = await bot.join_voice_channel(after.voice.voice_channel)
         else:
-            await client.voice_channel.move_to(after.voice.voice_channel)
+            await bot.voice_channel.move_to(after.voice.voice_channel)
         if after.server_permissions.administrator:
             music = 'https://goxash.tk/arthas.mp3' # 'omae wa - mo shindeiru.mp3'
         else:
             music = 'AND HIS NAME IS - JOHN CENA.mp3'
-        client.player = client.voice_channel.create_ffmpeg_player(music)
-        client.player.start()
+        bot.player = bot.voice_channel.create_ffmpeg_player(music)
+        bot.player.volume = VOLUME
+        await asyncio.sleep(GREETINGS_DELAY)
+        bot.player.start()
 
-
-client.voice_channel = None
-client.player = None
-client.run(TOKEN)
+bot.voice_channel = None
+bot.player = None
+bot.run(TOKEN)
